92e45f55e06f3d1e848ef58ded8674671784081b
Jonathan LAURENT <johjo@ytreza.org>
2023-03-27 00:30:52 +0200

    add missing tests

--- arenas/dojo-ytreza-dev/challenges/ms05/julius/test/main.test.ts
+++ arenas/dojo-ytreza-dev/challenges/ms05/julius/test/main.test.ts
@@ -1,8 +1,13 @@
 import { minesweeper } from "..";
 
 describe("minesweeper", () => {
-  it("should solve one empty cell", () => {
-    const solved_field = minesweeper(".")
-    expect(solved_field).toEqual("0")
-})
+  it("should solve empty field", () => {
+    const solved_field = minesweeper("")
+    expect(solved_field).toEqual("")
+  })
+
+  it("should solve one mined cell", () => {
+    const solved_field = minesweeper("*")
+    expect(solved_field).toEqual("*")
+  })
 })
--- arenas/dojo-ytreza-dev/challenges/ms06/julius/test/main.test.ts
+++ arenas/dojo-ytreza-dev/challenges/ms06/julius/test/main.test.ts
@@ -1,8 +1,18 @@
 import { minesweeper } from "..";
 
 describe("minesweeper", () => {
+  it("should solve empty field", () => {
+    const solved_field = minesweeper("")
+    expect(solved_field).toEqual("")
+  })
+
   it("should solve one empty cell", () => {
     const solved_field = minesweeper(".")
     expect(solved_field).toEqual("0")
-})
+  })
+
+  it("should solve one mined cell", () => {
+    const solved_field = minesweeper("*")
+    expect(solved_field).toEqual("*")
+  })
 })
