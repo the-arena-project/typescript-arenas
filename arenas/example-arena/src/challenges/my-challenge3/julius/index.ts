4147d50ffb78a1d2558f8f120968a56435ca324f
Aurelien Brabant <contact@aurelienbrabant.fr>
2023-03-31 22:11:51 +0200

    :sparkles: Update example-arena to test CI

---
+++ arenas/example-arena/src/challenges/my-challenge3/julius/index.ts
@@ -0,0 +1,7 @@
+// This is the reference implementation.
+// You can use it to test your solution.
+// It is not strictly mandatory, but it is recommended.
+
+export const myChallenge = () => {
+  return 42;
+};
