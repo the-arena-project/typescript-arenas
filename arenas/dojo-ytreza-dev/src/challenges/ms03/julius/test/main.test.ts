10e9356e29ffe2c7c74012b61cd0e9fca9f1de6f
Kevin Piacentini <kevinpiac@gmail.com>
2023-04-01 13:26:44 +0200

    Remove jo's repo since tests don't pass

--- arenas/dojo-ytreza-dev/src/challenges/ms03/julius/test/main.test.ts
+++
@@ -1,8 +0,0 @@
-import { minesweeper } from "..";
-
-describe("minesweeper", () => {
-  it("should solve one empty cell", () => {
-    const solved_field = minesweeper(".")
-    expect(solved_field).toEqual("0")
-})
-})
