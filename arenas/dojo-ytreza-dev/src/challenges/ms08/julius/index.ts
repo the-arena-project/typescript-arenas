d1f3047620dfef1d1b52ed4dfc20485eb7c29e23
Jonathan LAURENT <johjo@ytreza.org>
2023-04-13 22:59:33 +0200

    add code to pass ms08

--- arenas/dojo-ytreza-dev/src/challenges/ms08/julius/index.ts
+++ arenas/dojo-ytreza-dev/src/challenges/ms08/julius/index.ts
@@ -2,19 +2,14 @@ type MinesweeperFn = (field: string) => string;
 
 export const minesweeper: MinesweeperFn = (field: string) => {
     let solvedField = '';
-    if (field.length > 0) {
-        if (field[0] == '*') {
+    let x = 0;
+    while (field.length > x) {
+        if (field[x] == '*') {
             solvedField += '*';
         } else {
             solvedField += '0';
         }
-    }
-    if (field.length > 1) {
-        if (field[0] == '*') {
-            solvedField += '*';
-        } else {
-            solvedField += '0';
-        }
-    }
+        x += 1
+    }    
     return solvedField;
 };
