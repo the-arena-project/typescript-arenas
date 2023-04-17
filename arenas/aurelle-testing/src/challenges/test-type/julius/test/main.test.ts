6f47e0eefe21ecfd25b96556052a9072e2e3bc3d
Aurelien Brabant <contact@aurelienbrabant.fr>
2023-04-17 15:47:44 +0200

    :sparkles: Do not include ts-toolbelt test in jest test
    g

--- arenas/aurelle-testing/src/challenges/test-type/julius/test/main.test.ts
+++ arenas/aurelle-testing/src/challenges/test-type/julius/test/main.test.ts
@@ -1,15 +1,5 @@
-import { Test } from 'ts-toolbelt';
-import { Book as ActualBook } from '..';
-
-export interface RefBook {
-    title: string;
-    authorName: string;
-}
-
-const { checks, check } = Test;
-
 describe('test-type', () => {
-    it('should be the expected book type', () => {
-        checks([check<ActualBook, RefBook, Test.Pass>()]);
+    it('should pass test types', () => {
+        expect(true).toBe(true);
     });
 });
