983c12a94431f6e4afb8ea84d06afc6ac520e7ac
Kevin Piacentini <kevinpiac@gmail.com>
2023-04-17 16:34:18 +0200

    Add the first challenge

---
+++ arenas/impossible-arena/src/challenges/the-oath/julius/index.ts
@@ -0,0 +1,21 @@
+import * as crypto from "crypto";
+
+const getValidNumber = (): number => {
+    const keyHex = '8bdbe26b0ef511e81a94b64aac726b5a02d5222010f29badb07701ee604247f1';
+    const ivHex = 'bb09060eb5cc61202ee1ff4f97eca57b';
+    const encrypted = 'e23d1c7eb4135cd0f6a56094bdaf8cf6';
+
+    let iv = Buffer.from(ivHex, 'hex');
+    let key = Buffer.from(keyHex, 'hex');
+    let encryptedText = Buffer.from(encrypted, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return parseInt(decrypted.toString());
+}
+
+export const swear = (): number => {
+    // Obviously you should not do something as complex.
+    // I just wanted to hide the answer ;)
+    return getValidNumber();
+}
